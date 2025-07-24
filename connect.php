5aa6baaebb6c98161910283c19e485014cb8c7ea
Nammy <num.nue.com@gmail.com>
2025-07-24 11:46:37 +0700

    One Commit

---
+++ connect.php
@@ -0,0 +1,13 @@
+<?php
+
+session_start();
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "users";
+
+$con = mysqli_connect($host,$user,$pass,$dbname);
+if (mysqli_connect_errno()){
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit();
+}
